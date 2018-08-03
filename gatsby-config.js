3f1baf1b93baa6ca2c51fed3f62afecdb9963b4e
theoleanse <theoleanse@gmail.com>
2018-08-03 22:21:41 +0200

    some cms stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 'gatsby-plugin-netlify-cms',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images',
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <figure>
+      <img src="http://anna-paterson.com/content/1-home/img_3920.jpg" alt="" />
+    </figure>
+    <figure>
+      <img src="http://anna-paterson.com/content/1-home/img_0821.jpg" alt="" />
+    </figure>
   </div>
 )
 
