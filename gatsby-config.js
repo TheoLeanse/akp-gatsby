6b250ebeafd02b1b9489e801b2fb400f34804261
theoleanse <theoleanse@gmail.com>
2018-08-03 22:28:05 +0200

    delete gatsy source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet', 'gatsby-plugin-netlify-cms',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images`,
-        name: 'images',
-      }
-    }
-  ],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-netlify-cms'],
 }
