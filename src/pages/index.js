5c421c2031f34eef6f7ab29804af8e9ba5fd628d
theoleanse <theoleanse@gmail.com>
2018-08-03 23:04:34 +0200

    tryin somethin out

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <figure>
-      <img src="http://anna-paterson.com/content/1-home/img_3920.jpg" alt="" />
-    </figure>
-    <figure>
-      <img src="http://anna-paterson.com/content/1-home/img_0821.jpg" alt="" />
-    </figure>
-  </div>
-)
+const IndexPage = () => <div />
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const SecondPage = () => (
-  <div>
-  </div>
-)
+const SecondPage = ({ data }) => {
+  console.log(data)
+  const { edges: images } = data.allFile
+  return (
+    <div>{images.map(({ node: image }) => <img src={image.publicURL} />)}</div>
+  )
+}
 
 export default SecondPage
+
+export const pageQuery = graphql`
+  query foo {
+    allFile {
+      edges {
+        node {
+          publicURL
+        }
+      }
+    }
+  }
+`
