d9ff36801ec1576e6ce3fe2f0169cad28b5869a9
theoleanse <theoleanse@gmail.com>
2018-08-04 08:29:44 +0200

    try to get markdown content for pages...

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,18 @@ module.exports = {
         name: 'images',
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
   ],
 }
